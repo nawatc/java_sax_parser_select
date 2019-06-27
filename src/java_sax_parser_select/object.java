e1ed404030eb86b10681658b0cf5e82c273602aa
nawat phongpasura <nawatc01@gmail.com>
2019-06-27 11:43:14 +0700

    change lib ,can read some of xml file

--- src/java_sax_parser_select/object.java
+++ src/java_sax_parser_select/object.java
@@ -17,7 +17,30 @@ public class object {
 	    public void set_title(String title) {
 	        this.title = title;
 	    }
-
+	    public String link() {
+	        return link;
+	    }
+	    public void set_link(String link) {
+	        this.link = link;
+	    }
+	    public String description() {
+	        return description;
+	    }
+	    public void set_description(String description) {
+	        this.description = description;
+	    }
+	    public String language() {
+	        return language;
+	    }
+	    public void set_language(String language) {
+	        this.language = language;
+	    }
+	    public String copyright() {
+	        return copyright;
+	    }
+	    public void set_copyright(String copyright) {
+	        this.copyright = copyright;
+	    }
 	    public int get_ttl() {
 	        return ttl;
 	    }
--- src/java_sax_parser_select/sax_parser.java
+++ src/java_sax_parser_select/sax_parser.java
@@ -1,11 +1,76 @@
 package java_sax_parser_select;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
- 
-public class sax_parser
-{
+
+public class sax_parser {
+  public static void main(String argv[]) {
+
+    try {
+
+	File fXmlFile = new File("C:\\Users\\Red\\Desktop\\region_daily_forecast.xml");
+	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	Document doc = dBuilder.parse(fXmlFile);
+	
+	doc.getDocumentElement().normalize();
+
+	System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+			
+	NodeList nList = doc.getElementsByTagName("channel");
+			
+	System.out.println("----------------------------");
+
+	for (int temp = 0; temp < nList.getLength(); temp++) {
+
+		Node nNode = nList.item(temp);
+				
+		System.out.println("\nCurrent Element :" + nNode.getNodeName());
+				
+		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+			Element eElement = (Element) nNode;
+
+			System.out.println("title : " + eElement.getElementsByTagName("title").item(0).getTextContent());
+			System.out.println("link : " + eElement.getElementsByTagName("link").item(0).getTextContent());
+			System.out.println("description : " + eElement.getElementsByTagName("description").item(0).getTextContent());
+			System.out.println("language : " + eElement.getElementsByTagName("language").item(0).getTextContent());
+			System.out.println("copyright : " + eElement.getElementsByTagName("copyright").item(0).getTextContent());
+
+		}
+	}
+	
+	System.out.println("----------------------------");
+	
+	NodeList nList_2 = doc.getElementsByTagName("item");
+	
+	for (int temp = 0; temp < nList_2.getLength(); temp++) {
+
+		Node nNode = nList_2.item(temp);
+				
+		System.out.println("\nCurrent Element :" + nNode.getNodeName());
+				
+		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+			Element eElement = (Element) nNode;
+
+			System.out.println("title : " + eElement.getElementsByTagName("title").item(0).getTextContent());
+			System.out.println("guid : " + eElement.getElementsByTagName("guid").item(0).getTextContent());
+			System.out.println("description : " + eElement.getElementsByTagName("description").item(0).getTextContent());
+
+		}
+	}
+	
+	
+	
+    } catch (Exception e) {
+	e.printStackTrace();
+    }
+  }
 
 }
